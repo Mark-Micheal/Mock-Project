0c5427085807cc34172308017ab59d3bb57d58cc
Mark <mark.abdelaziz@student.guc.edu.eg>
2019-02-08 13:54:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,37 @@
+var result = ["Lujine","Shiko","Adel","Ayman","Farid","Hosam","Mostafa","Omar","Yasser"]
 var number = 0;
-for (int i =0;i<10;i++){
+alert('Final result is:'+ result)
+if (number<10){
+    var num1=m1(number);
+    alert("m1 is working")
+}
+else if(number<20){
+    var num2=m2(number);
+    alert("m2 is working");
+}
+else if(number<30){
+    var num3=m3(number);
+    alert("m3 is working");
+}
+else if(number<40){
+    var num4=m4();
+    alert("m2 is working");
+}
+
+for(var i=0;i<result.length;i++){
+    console.log(result[i])
+}
+
+function m1(number){
 
 }
+function m2(number){
+    
+}
+function m3(number){
+    
+}
+function m4(number){
+    
+}
+
