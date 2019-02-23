29785338e4fa645eb7239314c3637ac2241dc352
Mark-Micheal <36182888+Mark-Micheal@users.noreply.github.com>
2019-02-23 23:56:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const express = require('express');   // Import express
-
 const admins = require('./routes/api/admins')
-
-const app = express()                 // Create the app
-app.use(express.json())               // Use it with post
-
-
-
-app.get('/' , (req,res) => {
-    res.send(`
-        <h1> Welcome </h1>
-        <a href="/api/admins">Admins</a>
-    `);
-})
-
-app.use('/api/admins' , admins)
+// Use it with post  app.use(express.json())  
+app.use('/admins' , admins)
 
 app.use((req,res) => {
     res.status(404).send({err:'error 404 object not found '});
 })
 
-const port = 3000;
-app.listen(port , () => console.log(`Server up and running on port ${port}`))
 
