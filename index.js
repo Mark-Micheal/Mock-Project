933d208f765a4aa5f34b0d2c22d93909947b5249
Mark <mark.abdelaziz@student.guc.edu.eg>
2019-02-23 22:35:55 +0200

    Uploading Main files
    index,package,routes/api and models

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+var express=require('express');
+var app=express();
+var externalEntities=require('./routes/api/externalEntities');
 
+app.use('/externalEntities',externalEntities);
+
+app.get('/',(req,res) =>{
+    res.send(`<h1>WelcomeEE</h1>`)
+  })
+  
+  app.listen(8000,()=>{
+    console.log('The server is running');
+  })
