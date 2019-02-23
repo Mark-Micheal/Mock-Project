581db3d34df5c4654cf9bd7bc8c722b2dfdf2fa1
Mark-Micheal <36182888+Mark-Micheal@users.noreply.github.com>
2019-02-24 00:03:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
+const express=require('express');
+const app=express();
+const externalEntities=require('./routes/api/externalEntities');
 const admins = require('./routes/api/admins')
-// Use it with post  app.use(express.json())  
+
+app.use('/externalEntities',externalEntities);
 app.use('/admins' , admins)
+
+app.get('/',(req,res) =>{
+    res.send(`<h1>WelcomeEE</h1>`)
+  })
 
 app.use((req,res) => {
     res.status(404).send({err:'error 404 object not found '});
 })
+
+  app.listen(8000,()=>{
+    console.log('The server is running');
+  })
+
+
+
+// Use it with post  app.use(express.json())  
+
+
