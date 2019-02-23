b449f20cf874e60bb1e8c187e5085604b86d0e95
Mark-Micheal <36182888+Mark-Micheal@users.noreply.github.com>
2019-02-23 23:59:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const admins = require('./routes/api/admins')
 // Use it with post  app.use(express.json())  
 app.use('/admins' , admins)
@@ -5,5 +27,3 @@ app.use('/admins' , admins)
 app.use((req,res) => {
     res.status(404).send({err:'error 404 object not found '});
 })
-
-
