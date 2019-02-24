c3401f1b7db89694c1d40b61cd720c48df78467a
Lujine Elfeky <lujineelfeky@gmail.com>
2019-02-24 12:01:47 +0200

    Some edits
    Changed the order of routes and modified some thing slightly

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express=require('express');
-const app=express();
-const externalEntities=require('./routes/api/externalEntities');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const externalEntities = require('./routes/api/externalEntities');
 const admins = require('./routes/api/admins')
 
-app.use('/externalEntities',externalEntities);
-app.use('/admins', admins)
-
-app.get('/',(req,res) =>{
+app.get('/',(req,res) => {
     res.send(`<h1>WelcomeEE</h1>
     <a href="/admins">Admins</a></br>
     <a href="/externalEntities">EE</a></br>`)
     
   })
 
+app.use('/api/externalEntities',externalEntities);
+app.use('/api/admins', admins);
 
 app.use((req,res) => {
     res.status(404).send({err:'error 404 object not found '});
 })
 
-  app.listen(8000,()=>{
-    console.log('The server is running');
-  })
+const port = 8000;
+app.listen(port, () => { console.log(`Server is running on port ${port}`) })
 
 
 
