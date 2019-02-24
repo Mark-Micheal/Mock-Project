e5ba2d94d3c1c4618b98c51e7cef83b99c4ff45d
Mohamed Adel <mohamedadel1551998@gmail.com>
2019-02-24 13:42:07 +0200

    Edits to the lawyer
    SHOW AND DELETE METHODS (DELETE FEHA HGA MSH MZBOTA )

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 const externalEntities = require('./routes/api/externalEntities');
 const admins = require('./routes/api/admins')
+const lawyers = require('./routes/api/lawyers')
+
 
 app.get('/',(req,res) => {
     res.send(`<h1>WelcomeEE</h1>
@@ -11,9 +14,14 @@ app.get('/',(req,res) => {
     <a href="/externalEntities">EE</a></br>`)
     
   })
-
+  // Direct routes to appropriate files 
+app.use('/api/lawyers', lawyers)
 app.use('/api/externalEntities',externalEntities);
 app.use('/api/admins', admins);
+// Handling 404
+app.use((req, res) => {
+  res.status(404).send({err: 'We can not find what you are looking for'});
+})
 
 app.use((req,res) => {
     res.status(404).send({err:'error 404 object not found '});
--- routes/api/lawyers.js
+++ routes/api/lawyers.js
@@ -1,1 +1,38 @@
+const express = require('express');
+const Joi = require('joi');
+const uuid = require('uuid');
+const router = express.Router();
 
+// Models
+const Lawyer = require('../../models/Lawyer');
+// temporary data created as if it was pulled out of the database ...
+const lawyers = [
+	new Lawyer('Barney', 30),
+	new Lawyer('Lilly', 27),
+	new Lawyer('Ted', 29),
+	new Lawyer('Marshal', 27),
+	new Lawyer('Robin', 28)
+];
+router.get('/', (req, res) => res.json({ data: lawyers }));
+
+// Get a certain lawyer
+router.get('/api/lawyers/:id', (req, res) => {
+    const lawyerId = req.params.id
+    const lawyer = lawyers.find(lawyer => lawyer.id === lawyerId)
+    res.send(lawyer)
+})
+//delete lawyer
+ 
+router.delete('/api/lawyers/:id', (req, res) => {
+    const lawyerId = req.params.id
+    const lawyer = lawyers.find_by_uuid(lawyer => lawyer.id===lawyerId)
+    const index = lawyers.indexOf(lawyer)
+    lawyers.splice(index,1)
+    res.send(lawyers)
+})
+
+
+
+
+
+module.exports=router ;
