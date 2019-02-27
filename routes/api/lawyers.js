3f3483c2cb910b9879179fcf24ad8568339e1e2f
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-02-27 12:41:04 +0200

    Update lawyers.js
    Added the create and update part of the Lawyer

--- routes/api/lawyers.js
+++ routes/api/lawyers.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require('express');
 const Joi = require('joi');
 const uuid = require('uuid');
@@ -13,7 +14,60 @@ const lawyers = [
 	new Lawyer('Marshal', 27),
 	new Lawyer('Robin', 28)
 ];
+
 router.get('/', (req, res) => res.json({ data: lawyers }));
+
+// Create a new lawyer
+router.post('/', (req, res) => {
+	const name = req.body.name;
+	const age = req.body.age;
+
+	if (!name) return res.status(400).send({ err: 'Name field is required' });
+	if (typeof name !== 'string') return res.status(400).send({ err: 'Invalid value for name' });
+	if (!age) return res.status(400).send({ err: 'Age field is required' });
+	if (typeof age !== 'number') return res.status(400).send({ err: 'Invalid value for age' });
+
+	const newLawyer = {
+		name,
+		age,
+		id: uuid.v4(),
+	};
+	return res.json({ data: newLawyer });
+});
+
+
+router.post('/joi', (req, res) => {
+	const name = req.body.name
+	const age = req.body.age
+
+	const schema = {
+		name: Joi.string().min(3).required(),
+		age: Joi.number().required(),
+	}
+
+	const result = Joi.validate(req.body, schema);
+
+	if (result.error) return res.status(400).send({ error: result.error.details[0].message });
+
+	const newLawyer = {
+		name,
+		age,
+		id: uuid.v4(),
+	};
+	return res.json({ data: newLawyer });
+});
+
+
+//Updating a Lawyer record.
+router.put('/', (req, res) => {
+    const id = req.body.id;
+    const newName = req.body.name;
+    const newAge = req.body.age;
+    const lawyerToBeUpdated = lawyers.find_by_uuid(Lawyer => Lawyer.id === id);
+    lawyerToBeUpdated.name = newName;
+    lawyerToBeUpdated.age = newAge;
+    res.send(lawyers);
+})
 
 // Get a certain lawyer
 router.get('/api/lawyers/:id', (req, res) => {
@@ -31,8 +85,4 @@ router.delete('/api/lawyers/:id', (req, res) => {
     res.send(lawyers)
 })
 
-
-
-
-
-module.exports=router ;
+module.exports=router;
